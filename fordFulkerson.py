77253f27c9acc74f49400f67cc0a4c436e378852
GabFreitasDev <gabriel.fo.br@gmail.com>
2025-06-17 16:37:31 -0300

    Update fordFulkerson.py

--- fordFulkerson.py
+++ fordFulkerson.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 class Grafo:
 
     def __init__(self, grafo):
-        self.grafo = grafo  # grafo residual
+        self.grafo = grafo 
         self.ROW = len(grafo)
 
     def DFS(self, s, t, parent, visited):
